ad3f215c050718d3e1ec3a9cb97e259d0ad46e4f
Andres Parra <byandrev@gmail.com>
2025-12-14 19:34:51 -0500

    feat(infra): implement getAll method for fetching EstadoPedido by userId and pedidoId

--- src/main/java/com/pragma/powerup/infrastructure/out/mongo/adapter/EstadoPedidoAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/mongo/adapter/EstadoPedidoAdapter.java
@@ -2,11 +2,15 @@ package com.pragma.powerup.infrastructure.out.mongo.adapter;
 
 import com.pragma.powerup.domain.model.EstadoPedidoModel;
 import com.pragma.powerup.domain.spi.IEstadoPedidoPersistencePort;
+import com.pragma.powerup.infrastructure.exception.ResourceNotFound;
 import com.pragma.powerup.infrastructure.out.mongo.entity.EstadoPedidoEntity;
 import com.pragma.powerup.infrastructure.out.mongo.mapper.IEstadoPedidoEntityMapper;
 import com.pragma.powerup.infrastructure.out.mongo.repository.IEstadoPedidoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,6 +24,17 @@ public class EstadoPedidoAdapter implements IEstadoPedidoPersistencePort {
     public EstadoPedidoModel save(EstadoPedidoModel estadoPedido) {
         EstadoPedidoEntity entity = mapper.toEntity(estadoPedido);
         return mapper.toModel(repository.save(entity));
+    }
+
+    @Override
+    public List<EstadoPedidoModel> getAll(Long userId, Long pedidoId) {
+        List<EstadoPedidoEntity> listEntity = repository.findAllByClienteIdAndPedidoId(userId, pedidoId);
+
+        if (listEntity.isEmpty()) {
+            throw new ResourceNotFound("El pedido " + pedidoId + " no existe");
+        }
+
+        return listEntity.stream().map(mapper::toModel).collect(Collectors.toList());
     }
 
 }
