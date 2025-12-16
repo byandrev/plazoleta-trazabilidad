9d82a6161d626749eb5f06912c72bf49b89c2840
Andres Parra <byandrev@gmail.com>
2025-12-16 16:46:08 -0500

    feat(domain): add @Builder annotation to PaginationInfo and PedidoTimeModel

--- src/main/java/com/pragma/powerup/domain/model/PaginationInfo.java
+++ src/main/java/com/pragma/powerup/domain/model/PaginationInfo.java
@@ -1,12 +1,14 @@
 package com.pragma.powerup.domain.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class PaginationInfo {
 
     private int page;
--- src/main/java/com/pragma/powerup/domain/model/PedidoTimeModel.java
+++ src/main/java/com/pragma/powerup/domain/model/PedidoTimeModel.java
@@ -1,6 +1,7 @@
 package com.pragma.powerup.domain.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Data
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Builder
 public class PedidoTimeModel {
 
     private Long pedido;
