c952044d21023489e6f46199e6b3f1a3a4532d14
Andres Parra <byandrev@gmail.com>
2025-12-15 20:39:00 -0500

    feat(infra): add getTimePedidos endpoint with authorization

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/EstadoPedidoController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/EstadoPedidoController.java
@@ -1,7 +1,10 @@
 package com.pragma.powerup.infrastructure.input.rest;
 
 import com.pragma.powerup.application.dto.request.EstadoPedidoRequestDto;
+import com.pragma.powerup.application.dto.request.PaginationRequestDto;
 import com.pragma.powerup.application.dto.response.EstadoPedidoResponseDto;
+import com.pragma.powerup.application.dto.response.PaginationResponseDto;
+import com.pragma.powerup.application.dto.response.PedidoTimeResponseDto;
 import com.pragma.powerup.application.handler.IEstadoPedidoHandler;
 import com.pragma.powerup.infrastructure.input.rest.response.CustomResponse;
 import com.pragma.powerup.infrastructure.out.security.models.CustomUserDetail;
@@ -13,6 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,6 +46,7 @@ public class EstadoPedidoController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Traceability list")
     })
+    @PreAuthorize("hasRole('CLIENTE')")
     @GetMapping("/{pedidoId}")
     public ResponseEntity<CustomResponse<List<EstadoPedidoResponseDto>>> getAll(
             @PathVariable Long pedidoId,
@@ -52,6 +57,23 @@ public class EstadoPedidoController {
         CustomResponse<List<EstadoPedidoResponseDto>> response = CustomResponse.<List<EstadoPedidoResponseDto>>builder()
                 .status(HttpStatus.OK.value())
                 .data(estadoPedidoHandler.getAll(userId, pedidoId))
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "Get tiempos de pedidos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de tiempos de pedidos")
+    })
+    @PreAuthorize("hasRole('PROPIETARIO')")
+    @GetMapping("/pedidos")
+    public ResponseEntity<CustomResponse<PaginationResponseDto<PedidoTimeResponseDto>>> getTimePedidos(
+            @Valid PaginationRequestDto paginationRequest
+    ) {
+        CustomResponse<PaginationResponseDto<PedidoTimeResponseDto>> response = CustomResponse.<PaginationResponseDto<PedidoTimeResponseDto>>builder()
+                .status(HttpStatus.OK.value())
+                .data(estadoPedidoHandler.getTimePedidos(paginationRequest))
                 .build();
 
         return ResponseEntity.ok(response);
