eec1f5ca094294ba6fe721275f61f439257809f3
Andres Parra <byandrev@gmail.com>
2025-12-16 19:53:45 -0500

    feat(infra): update repository to include ranking by employee

--- src/main/java/com/pragma/powerup/infrastructure/out/mongo/repository/IEstadoPedidoRepository.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/mongo/repository/IEstadoPedidoRepository.java
@@ -1,5 +1,6 @@
 package com.pragma.powerup.infrastructure.out.mongo.repository;
 
+import com.pragma.powerup.domain.model.EmpleadoTiempoModel;
 import com.pragma.powerup.domain.model.PedidoTimeModel;
 import com.pragma.powerup.infrastructure.out.mongo.entity.EstadoPedidoEntity;
 import org.springframework.data.domain.Pageable;
@@ -43,23 +44,39 @@ public interface IEstadoPedidoRepository extends MongoRepository<EstadoPedidoEnt
     List<PedidoTimeModel> getTimeByPedidos(Long restauranteId, Pageable pageable);
 
     @Aggregation(pipeline = {
-            "{ '$match': { 'id_pedido': ?0, 'estado_nuevo': { '$in': ['PENDIENTE', 'ENTREGADO'] } } }",
+            "{ '$match': { 'id_restaurante': ?0, 'id_pedido': { '$ne': null }, 'estado_nuevo': { '$in': ['PENDIENTE', 'ENTREGADO'] } } }",
+            "{ '$group': { '_id': '$id_pedido' } }",
+            "{ '$project': { '_id': 0, 'idEmpleado': '$id_empleado' } }",
+            "{ '$group': { '_id': '$idEmpleado' } }",
+            "{ '$count': 'total' }"
+    })
+    long countTotalEmpleadosRanking(Long restauranteId);
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'id_restaurante': ?0, 'id_pedido': { '$ne': null }, 'estado_nuevo': { '$in': ['PENDIENTE', 'ENTREGADO'] } } }",
 
             "{ '$group': { " +
                     "'_id': '$id_pedido', " +
-                    "'fechaPreparacion': { '$min': { '$cond': [ { '$eq': ['$estado_nuevo', 'PENDIENTE'] }, '$fecha', null ] } }, " +
-                    "'fechaEntregado': { '$max': { '$cond': [ { '$eq': ['$estado_nuevo', 'ENTREGADO'] }, '$fecha', null ] } }, " +
-                    "'pedido': { '$first': '$id_pedido' } " +
+                    "'empleado': { '$last': '$id_empleado' }, " +
+                    "'fechaInicio': { '$min': { '$cond': [ { '$eq': ['$estado_nuevo', 'PENDIENTE'] }, '$fecha', null ] } }, " +
+                    "'fechaFin': { '$max': { '$cond': [ { '$eq': ['$estado_nuevo', 'ENTREGADO'] }, '$fecha', null ] } }, " +
+                    "} }",
+
+            "{ '$group': { " +
+                    "'_id': '$empleado', " +
+                    "'tiempoMedioSegundos': { '$avg': { '$divide': [{ '$subtract': ['$fechaFin', '$fechaInicio'] }, 1000] } } " +
                     "} }",
 
             "{ '$project': { " +
                     "'_id': 0," +
-                    "'fechaInicio': '$fechaPreparacion'," +
-                    "'fechaFin': '$fechaEntregado'," +
-                    "'tiempo': { '$divide': [ { '$subtract': ['$fechaEntregado', '$fechaPreparacion'] }, 1000 ] }," +
-                    "'pedido': '$pedido'," +
-                    "} }"
+                    "'empleado': '$_id'," +
+                    "'tiempoMedioSegundos': '$tiempoMedioSegundos' " +
+                    "} }",
+
+            "{ '$sort': { 'tiempoMedioSegundos': 1 } }",
+            "{ '$skip': ?#{#pageable.offset} }",
+            "{ '$limit': ?#{#pageable.pageSize} }"
     })
-    PedidoTimeModel getTimeByPedidoId(Long pedidoId);
+    List<EmpleadoTiempoModel> getRankingTiempoByEmpleado(Long restauranteId, Pageable pageable);
 
 }
